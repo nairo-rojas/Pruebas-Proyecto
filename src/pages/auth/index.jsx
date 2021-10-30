b8ace4da446b449866c5c775734c67b1c9ed8cac
nairo-rojas <86504357+nairo-rojas@users.noreply.github.com>
2021-10-30 17:03:15 -0500

    Add files via upload

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,1 +1,13 @@
+//import { useDarkMode } from 'context/darkMode';
+import React from 'react';
 
+const Index = () => {
+  //const { darkMode } = useDarkMode();
+  return (
+    <div className='flex h-full bg-gray-900'>
+      Contenido de la Gestión de Ventas
+    </div>
+  );
+};
+
+export default Index;
