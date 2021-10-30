7d028c4cf73dc1109d72017cc39bdeb703bf5ac3
nairo-rojas <86504357+nairo-rojas@users.noreply.github.com>
2021-10-30 17:09:34 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,86 @@
 
-import Index from 'pages';
-import BorderCollieInfoPage from 'pages/border-collie';
-import RhodesianInfoPage from 'pages/rhodesian';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-}from 'react-router-dom';
-import 'styles/App.css';
-import Layout from 'layouts/layout';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './styles/App.css';
+import PrivateLayout from 'layouts/PrivateLayout';
+import AuthLayout from 'layouts/AuthLayout';
+import PublicLayout from 'layouts/PublicLayout';
+import Index from 'pages/index';
+import Admin from 'pages/admin/Index';
+import Productos from 'pages/admin/Productos';
+import Usuarios from 'pages/admin/Usuarios';
+import Ventas from 'pages/admin/Ventas';
+import Login from 'pages/auth/login';
+import Registro from 'pages/auth/registro';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { UserContext } from 'context/userContext';
+import PrivateRoute from 'components/PrivateRoute';
+
 
 function App() {
+  const [userData, setUserData] = useState({});
   return (
-    <div classNameName="App">
+    <Auth0Provider
+    domain="gestion-ventas-ants.us.auth0.com"
+        clientId="fiWmCl0FnahkDEsEgeqKEG06XDzfc9OF"
+       redirectUri='http://localhost:3000/admin'
+        audience='api-autenticacion-gestion-ventas'
+        >
+    <div className="App">
+      <UserContext.Provider value={{userData, setUserData}}>
       <Router>
-          <Layout>
         <Switch>
-          <Route path= '/Rhodesian'>
-            <RhodesianInfoPage/>
+          <Route path={['/admin/', '/admin/Productos', '/admin/Usuarios', '/admin/Ventas']}>
+            <PrivateLayout>
+              <Switch>
+                <Route path='/admin/Productos'>
+                  <PrivateRoute roleList = {['admin', 'vendedor']}>
+                <Productos />
+                </PrivateRoute>
+                 </Route>
+                <Route path='/admin/Usuarios'>
+                <PrivateRoute roleList={['admin']}>
+                  <Usuarios />
+                  </PrivateRoute>
+                </Route>
+                <Route path='/admin/Ventas'>
+                  <PrivateRoute roleList = {['admin', 'vendedor']}>
+                  <Ventas />
+                  </PrivateRoute>
+                </Route>
+                <Route path='/admin'>
+                  <Admin />
+              </Route>
+              </Switch>
+            </PrivateLayout>
           </Route>
-          <Route path= '/Border'>
-            <BorderCollieInfoPage/>
+          <Route path={['/Login', '/Registro']}>
+            <AuthLayout>
+              <Switch>
+                <Route path='/Login'>
+                  <Login />
+                </Route>
+                <Route path='/Registro'>
+                  <Registro />
+                </Route>
+              </Switch>
+            </AuthLayout>
           </Route>
-          <Route path= '/'>
-            <Index/>
+          <Route path={['/']}>
+            <PublicLayout>
+              <Switch>
+              <Route path='/'>
+                  <Index />
+                </Route>
+              </Switch>
+            </PublicLayout>
           </Route>
         </Switch>
-          </Layout>
       </Router>
+      </UserContext.Provider>
     </div>
+
+    </Auth0Provider>
   );
 }
 
